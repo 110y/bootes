e6cd3c6031d1da05462195035ba5a5129ed64d64
Yuki Ito <mrno110y@gmail.com>
2020-04-19 19:24:14 +0900

    add Endpoint

---
+++ internal/k8s/api/v1/endpoint_types.go
@@ -0,0 +1,41 @@
+package v1
+
+import (
+	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+var _ EnvoyResource = (*Endpoint)(nil)
+
+// EndpointList contains a list of Endpoint
+type EndpointList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []*Endpoint `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Endpoint is the Schema for the endpoints API
+// +k8s:openapi-gen=true
+type Endpoint struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec EndpointSpec
+}
+
+type EndpointSpec struct {
+	WorkloadSelector *WorkloadSelector `json:"workloadSelector,omitempty"`
+	Config           *envoyapi.ClusterLoadAssignment
+}
+
+func (l *Endpoint) GetWorkloadSelector() *WorkloadSelector {
+	return l.Spec.WorkloadSelector
+}
+
+func init() {
+	SchemeBuilder.Register(&Endpoint{}, &EndpointList{})
+}
